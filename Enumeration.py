e8940bfa5d7a68670d9a14d15145f4f9b17a4852
junxin126 <11332400@qq.com>
2018-09-09 21:40:25 +0800

    3.1.3

--- Enumeration.py
+++ Enumeration.py
@@ -17,11 +17,3 @@ for a in range(0, 1001):
 end_time = time.time()
 print ("计算结束，用时%d秒。" % (end_time - start_time))
 
-# 排列一个列表，sort()方法并不返回列表而是NONE
-li = [0, 100, 66, 44, 78, 99, 22]
-li.sort()
-print li
-# reverse()倒排序
-li.reverse()
-print li
-
